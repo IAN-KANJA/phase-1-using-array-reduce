777ad99f61a84f360035e90b8fc78ac9d6d70be6
iannjau <lantanacreations@gmail.com>
2024-04-16 20:43:43 +0300

    add my file

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = 31;
+const sumWithInitial = batteryBatches.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  sumWithInitial,
+);
 
-// Code your solution here
+console.log(sumWithInitial)
